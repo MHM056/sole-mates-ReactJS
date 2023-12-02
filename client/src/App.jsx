a39f678363020342eb34e9360418e91b133846ac
MHM056 <mihailmarholev@gmail.com>
2023-12-03 00:37:25 +0200

    Created Login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
 import { Home } from "./components/home/Home";
 import { Register } from "./components/register/Register";
+import { Login } from "./components/login/Login";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
 
       </div>
