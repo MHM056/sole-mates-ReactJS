616a0ee45c7bfe64a9ff781a9e130fcd24f2dcdf
MHM056 <mihailmarholev@gmail.com>
2023-12-06 19:52:31 +0200

    Updated Catalog component
    - Added state for shoes
    - Added useEffect to get all shoes

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
+import * as shoeService from "../../services/shoeService";
+import { ItemCard } from "./item-card/ItemCard";
+
 export const Catalog = () => {
+    const [shoes, setShoes] = useState([]);
+
+    useEffect(() => {
+        shoeService.getAll()
+        .then(setShoes);
+    }, []);
+
     return (
         <section id="dashboard">
             <h2>Collectibles</h2>
             <ul className="card-wrapper">
                 {/* <!-- Display a li with information about every post (if any)--> */}
-
+                {shoes.map(shoe => <ItemCard key={shoe._id} {...shoe}/>)}
             </ul>
 
-            {/* <!-- Display an h2 if there are no posts --> */}
-            <h2>There are no items added yet.</h2>
+            {shoes.length === 0 && <h2>There are no items added yet.</h2>}
         </section>
     );
 };
