f3d2e3c30502696d56db231f638a501e5c3097dd
MHM056 <mihailmarholev@gmail.com>
2023-12-07 00:31:45 +0200

    Update ShoeDetails.jsx
    - Added controlled values;
    - Only author can see Edit and Delete buttons;
    - Added functionality to the component

--- client/src/components/shoe-details/ShoeDetails.jsx
+++ client/src/components/shoe-details/ShoeDetails.jsx
@@ -1,26 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import * as shoeService from "../../services/shoeService";
+import AuthContext from "../../contexts/authContext";
+
 export const ShoeDetails = () => {
+    const { userId } = useContext(AuthContext);
+    const { shoeId } = useParams();
+    const [pair, setPair] = useState({});
+    const isAuthor = userId === pair._ownerId;
+
+
+    useEffect(() => {
+        shoeService.getOne(shoeId)
+            .then(setPair);
+    }, [shoeId]);
+
     return (
         <section id="details">
             <div id="details-wrapper">
                 <p id="details-title">Shoe Details</p>
                 <div id="img-wrapper">
-                    <img src="./images/travis.jpg" alt="example1" />
+                    <img src={pair.imageUrl} alt={pair.model} />
                 </div>
                 <div id="info-wrapper">
-                    <p>Brand: <span id="details-brand">Air Jordan</span></p>
+                    <p>Brand: <span id="details-brand">{pair.brand}</span></p>
                     <p>
-                        Model: <span id="details-model">1 Retro High TRAVIS SCOTT</span>
+                        Model: <span id="details-model">{pair.model}</span>
                     </p>
-                    <p>Release date: <span id="details-release">2019</span></p>
-                    <p>Designer: <span id="details-designer">Travis Scott</span></p>
-                    <p>Value: <span id="details-value">2000</span></p>
+                    <p>Release date: <span id="details-release">{pair.release}</span></p>
+                    <p>Designer: <span id="details-designer">{pair.designer}</span></p>
+                    <p>Value: <span id="details-value">{pair.value}</span></p>
                 </div>
 
                 {/* <!--Edit and Delete are only for creator--> */}
-                <div id="action-buttons">
-                    <a href="" id="edit-btn">Edit</a>
-                    <a href="" id="delete-btn">Delete</a>
-                </div>
+                {isAuthor && (
+                    <div id="action-buttons">
+                        <a href="" id="edit-btn">Edit</a>
+                        <a href="" id="delete-btn">Delete</a>
+                    </div>
+                )}
             </div>
         </section>
     );
