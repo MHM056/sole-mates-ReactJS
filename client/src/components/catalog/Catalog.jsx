eb345fcab061cea966facdb9d95185c67907d8d9
MHM056 <mihailmarholev@gmail.com>
2023-12-06 19:32:43 +0200

    Create Catalog component

---
+++ client/src/components/catalog/Catalog.jsx
@@ -0,0 +1,14 @@
+export const Catalog = () => {
+    return (
+        <section id="dashboard">
+            <h2>Collectibles</h2>
+            <ul className="card-wrapper">
+                {/* <!-- Display a li with information about every post (if any)--> */}
+
+            </ul>
+
+            {/* <!-- Display an h2 if there are no posts --> */}
+            <h2>There are no items added yet.</h2>
+        </section>
+    );
+};
