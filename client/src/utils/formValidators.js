e65c7937cb0196d056deea0b223bde4292370deb
MHM056 <mihailmarholev@gmail.com>
2023-12-17 00:40:05 +0200

    Update formValidators.js

--- client/src/utils/formValidators.js
+++ client/src/utils/formValidators.js
@@ -1,7 +1,6 @@
 export const validateRegister = (formValues) => {
 
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const result = {};
 
     if (!formValues.email.match(emailPattern)) {
         alert('Invalid email');
@@ -10,6 +9,11 @@ export const validateRegister = (formValues) => {
 
     if (formValues.password === '') {
         alert('Password is required');
+        return;
+    }
+    
+    if (formValues.password.length < 4 || formValues.password.length > 10) {
+        alert('Password should be between 4 and 10 characters');
         return;
     }
 
